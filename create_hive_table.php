e8a9bf91601f0f644c8bc20c9cff0ce9994ef4ae
Dat Nguyen <datzilla@gmail.com>
2018-03-26 15:42:02 +1100

    update

--- create_hive_table.php
+++ create_hive_table.php
@@ -13,9 +13,11 @@ $sep = null;
 $dbname = 'default';
 $tbname = null;
 $withheader = false;
-$minimum_col = 3;
+$escapesep = false;
+$minimum_col = 1;
 $dropexists = false;
 $withquotes = false;
+$columnprefix = 'V';
 $temp_file = fopen ("tmp.hql", "w");
 
 $usage_msg = '--pf : parent folder containing the text or gz files on hdfs. Do not add trailing /' . PHP_EOL;
@@ -23,10 +25,13 @@ $usage_msg .= '--db : the name of the database in hive, if not specified, it wil
 $usage_msg .= '--tb : the name of the table in hive database. If not specified, the script will not run.' . PHP_EOL;
 $usage_msg .= '--drop : drops the table before creating.' . PHP_EOL;
 $usage_msg .= '--sep : the column separator, for tab you do not need to specify \\ before the t. Just simple t. Same for pipe characters' . PHP_EOL;
+$usage_msg .= '--escapesep : if the seperator is | or ", the character needs to be escaped. this switch is then used.' . PHP_EOL;
 $usage_msg .= '--withheader : treats the first line of the text dataset as the header row. default is off' . PHP_EOL;
 $usage_msg .= '--withquotes : treats double quotes as a string value wrapper. default is off' . PHP_EOL;
 $usage_msg .= '--mincol : specifies the minimum number of columns in the text dataset. Script does not run if it is not met. Default is 3' . PHP_EOL;
+$usage_msg .= '--columnprefix : this will only work when there\'s no column header specified' . PHP_EOL;
 $usage_msg .= 'Example: php create_hive_table_single.php --db "dats_experiment" --tb "fixed_dart" --pf "/user/dat.nguyen/fixed_dart" --sep "," --withheader --withquotes --drop --mincol 3' . PHP_EOL;
+$usage_msg .= 'Alternative: php create_hive_table_single.php --db "dats_experiment" --tb "fixed_dart" --pf "/user/dat.nguyen/fixed_dart" --sep "," --columnprefix "FDC" --withquotes --drop --mincol 3' . PHP_EOL;
 
 if (count ($argv) > 1){
     for ($i = 0; $i < count ($argv); $i++) {
@@ -72,6 +77,14 @@ if (count ($argv) > 1){
             $dropexists = true;
         }
 
+	if ($argv[$i]== '--colprefix'){
+	   $columnprefix = $argv[$i + 1];
+	}
+
+	if ($argv[$i] == '--escapesep') {
+	   $escapesep = true;
+	}
+
     }
 }
 
@@ -89,11 +102,17 @@ if (
 
         $attribute_str = null;
         foreach ($outlines as $line) {
-
-            $columns = explode ($sep, $line);
+	    $newsep = "";
+	    if ($sep=="t") {
+		 $newsep = "\t";
+	    }elseif ($sep=="n") {
+		$newsep = "\n";
+	    } else {
+		$newsep = $sep;
+	    }
+            $columns = explode ($newsep, $line);
             $total_columns = count ($columns);
-            
-            if ($total_columns > $minimum_col){
+	    if ($total_columns >= $minimum_col){
                 // let's create the column headers with the first rows name
                 if ($withheader == true) {
                     for ($k = 0; $k < $total_columns; $k++) {
@@ -111,11 +130,16 @@ if (
                         $columns [$k] = "" . str_replace('\\', '_', $columns [$k]);
                         $columns [$k] = "" . str_replace(' ', '_', $columns [$k]);
 
-                        $columns [$k] = 'f'. $columns[$k]  . " STRING";
+                        // if there's _ at the front and the end of the string, just remove it and also tidy up when there's more than __.
+                        $columns [$k] = "" . str_replace('__', '_', $columns [$k]);
+                        $columns [$k] = ltrim ($columns[$k], '_');
+                        $columns [$k] = rtrim ($columns[$k], '_');
+
+                        $columns [$k] = '`'. $columns[$k]  . "` STRING";
                     }
                 } else {
                     for ($k = 0; $k < $total_columns; $k++) { 
-                        $columns [$k] = "V" . $k . " STRING";
+                        $columns [$k] = $columnprefix . $k . " STRING";
                     }
                 }
 
@@ -136,8 +160,10 @@ if (
             $hql .= $attribute_str;
             $hql .= ") ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde' ";
             $hql .= "WITH SERDEPROPERTIES ( ";
-            $hql .= ' "separator" = "\\' . $sep . '", ';
-            if ($withquotes == TRUE) $hql .= ' "quoteChar" = "\\""';
+            $hql .= ' "separatorChar" = "';
+	    if ($escapesep == TRUE) $hql .= '\\';
+	    $hql .= $sep . '"';
+            if ($withquotes == TRUE) $hql .= ', "quoteChar" = "\\""';
             $hql .= ' ) ';
             $hql .= "STORED AS TEXTFILE ";
             $hql .= "LOCATION '" . $foldername . "'";
@@ -146,25 +172,30 @@ if (
 
             fwrite ($temp_file, $hql);
 	        //echo $hql . PHP_EOL;
-            shell_exec ('hive -f "tmp.hql"');
+	    try {
+            	shell_exec ('hive -f "tmp.hql"');
+	    } catch (Exception $e) {
+		echo $e . PHP_EOL;
+	    }
             fclose ($temp_file);
 
         } else {
             
-            echo "something is wrong here? could not form attribute strings" . PHP_EOL;
+            echo "something is wrong here? column count is incorrect when trying to retrieve from data file" . PHP_EOL;
 
         }
 
     } else {
 
-        echo "something is wrong here? cannot read the text file" . PHP_EOL;
+        echo "something is wrong here? cannot read the data file" . PHP_EOL;
 
     }
 } else {
-    echo "something is wrong here? insufficient input parameters" . PHP_EOL;
+    echo "something is wrong here? insufficient inputs from command parameters" . PHP_EOL;
     echo $usage_msg;
 }
 
 
 ?>
+
 
